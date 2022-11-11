5851b85e734e46595253c467f698b9a61a3c8a9e
seturi <48719589+seturi@users.noreply.github.com>
2022-11-11 14:46:25 +0900

    Chapter 14.10 ~ 14.11

--- rpn_io.py
+++ rpn_io.py
@@ -15,21 +15,25 @@ def open_rpn_file():
     return a_list
 
 def do_prints(s):
-    '''문자열을 출력하여 PRINTS 지침을 수행한다.
+    '''문자열을 출력함수.
+    개행 문자 추가 없이 문자열 인수 s 출력.
     '''
     a_str = get_str(s)
     print(a_str, end=' ')
     
 def do_println(s):
-    '''PRINTLN 지침 수행: 만약 선택적인 문자열 인수가 주어지면
-    이를 출력하고 나서, 무조건 개행문자를 출력한다.
+    '''줄 출력 함수.
+    (선택적인) 문자열을 출력하고, 개행 문자를 추가한다.
     '''
     if s:
         do_prints(s)
-    print()
+        print()
     
 def get_str(s):
-    '''do_prints의 도우미 함수
+    '''문자열 도우미 함수 가져오기.
+    첫 번째 작은따옴표에서 마지막 작은따옴표까지
+    텍스트를 읽어온다. 읽을 텍스트가 없으면
+    빈 문자열을 반환한다.
     '''
     a = s.find("'")
     b = s.rfind("'")
@@ -38,15 +42,20 @@ def get_str(s):
     return s[a+1:b]
 
 def do_printvar(s, sym_tab):
-    '''PRINTVAR 지침 수행: 변수 이름이 문자열 s에 있는지 찾아본 후
-    심벌 테이블에 이 이름이 있는지 찾아본다.
+    '''변수 출력 함수.
+    메인 모듈에서 전달받은 sym_tab에서
+    변수를 찾아서 출력한다.
     '''
     wrd = s.split()[0]
     print(sym_tab[wrd], end=' ')
     
 def do_input(s, sym_tab):
-    '''INPUT 지침 수행: 사용자에게 값을 입력받은 후
-    심벌 테이블의 문자열 s 색인에 해당 값을 넣는다.
+    '''변수 입력 함수.
+    사용자에게 값을 입력받아서 변수에 넣은 후
+    심벌 테이블(sym_tab) 참조에 변수를 넣는다.
     '''
     wrd = input()
-    sym_tab[s] = float(wrd)
+    if '.' in wrd:
+        sym_tab[s] = float(wrd)
+    else:
+        sym_tab[s] = int(wrd)
